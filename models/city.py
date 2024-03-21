001798b1c43cb10b144ce2696e6d2dd7d2eb8a98
Fidelcom <okpara2040@gmail.com>
2024-03-21 03:58:33 +0000

    completed

--- models/city.py
+++ models/city.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey
+'''
+    Define the class City.
+'''
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
+from models.state import State
 
 
-class City(BaseModel):
-	"""Represents a city for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Cities.
-        name (sqlalchemy String): The name of the City.
-        state_id (sqlalchemy String): The state id of the City.
-    """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
-    name = Column(String(128), nullable=False)
+class City(BaseModel, Base):
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = "cities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id = ""
+        name = ""
