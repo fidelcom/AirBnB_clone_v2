915ff91af11eca49a35da0f501ef6073fc78e2a3
veeviyan <okpala_vivian@yahoo.com>
2024-04-04 17:53:31 +0100

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,7 @@
 using the function do_deploy"""
 from fabric.api import *
 import os
+from datetime import datetime
 
 
 env.hosts = ['100.26.168.29', '54.173.39.101']
@@ -13,14 +14,13 @@ env.key_filename = '~/.ssh/id_rsa'
 def do_deploy(archive_path):
     """Distributes an archive to web servers."""
 
-    if not os.path.exists(archive_path):
-        return False
-
     try:
-        for host in env.hosts:
-            put(archive_path, "/tmp/")
+        if not os.path.exists(archive_path):
+            return False
 
-        filename os.path.basename(archive_path)
+        put(archive_path, "/tmp/")
+
+        filename = os.path.basename(archive_path)
         filename_without_ext = os.path.splitext(filename)[0]
 
         release_dir = '/data/web_static/releases/{filename_without_ext}'
