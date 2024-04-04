d22c57731f8caa8cdb2d9571509a44c0847083ba
veeviyan <okpala_vivian@yahoo.com>
2024-04-04 12:23:52 +0100

    Write a Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt update
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+	    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+	http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" \
+	    | sudo tee /etc/apt/sources.list.d/nginx.list
+sudo apt update
+sudo apt install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo tee /data/web_static/releases/test/index.html >/dev/null <<EOF
+<html>
+  <head>
+  </head>
+  <body>
+    AirBnB Clone under construction
+  </body>
+</html>
+EOF
+
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo tee /etc/nginx/sites-available/default >/dev/null <<'EOF'
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	add_header X-Served-By $hostname;
+	root /var/www/html;
+	index index.html index.htm;
+
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		index index.html index.htm;
+	}
+
+	location /redirect_me {
+		return 301 http://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+		root /var/www/html;
+		internal
+	}
+}
+EOF
+sudo systemctl restart nginx
